4c0745f28c9dbd974638c9d448a07a7cf10a3abf
peregreenus <coldlycalculatedsoul@gmail.com>
2022-01-06 11:34:38 +0300

    classList and Event Delegation

--- classList.js
+++ classList.js
@@ -1,1 +1,13 @@
-console.log(123);
+const btns = document.querySelectorAll('button');
+const wrapper = document.querySelector('#first');
+
+// console.log(btns[0].classList.length);
+console.log(btns[1].classList.add('red'));
+console.log(btns[0].classList.remove('blue'));
+console.log(btns[0].classList.toggle('blue'));
+
+wrapper.addEventListener('click', (e) => {
+    if (e.target.matches('button')) {
+        console.log('Hello');
+    }
+});
