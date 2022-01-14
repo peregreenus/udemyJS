d2fec2c7f15b7a8fa1aca65d359159f832bf593a
peregreenus <coldlycalculatedsoul@gmail.com>
2022-01-14 11:25:45 +0300

    add: timer

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    // TABS ------------------------------------------------->
+
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -34,6 +36,59 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+
+    // TIMER ---------------------------------------------------->
+
+    const deadline = '2022-01-18';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - new Date();
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor(t / (1000 * 60)) % 60;
+        const seconds = Math.floor(t / 1000) % 60;
+
+        return {
+            total: t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+
+    function getZero(num) {
+        return num >= 0 && num < 10 ? `0${num}` : num;
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = document.querySelector('#days');
+        const hours = document.querySelector('#hours');
+        const minutes = document.querySelector('#minutes');
+        const seconds = document.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('.timer', deadline);
 
 
 
--- setTimeout_setInterval/script.js
+++ setTimeout_setInterval/script.js
@@ -44,3 +44,74 @@ function myAnimation() {
 }
 
 btn.addEventListener('click', myAnimation);
+
+// Date ----------------------------------------------------------------
+
+let now = new Date();
+
+console.log(now.toString()); // текущая дата и время
+console.log(now.getFullYear()); // год
+console.log(now.getDate()); // число
+
+console.log(now.getTime()); // разница между часовыми поясами
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+console.log(`Time: ${end - start}ms`);
+
+
+function getDateAgo(date, days) {
+    let dateCopy = new Date(date);
+
+    dateCopy.setDate(date.getDate() - days);
+    return dateCopy;
+}
+
+
+let date = new Date(2015, 0, 2);
+
+console.log(getDateAgo(date, 1));
+
+
+// Last Day of Month
+
+function getLastDayOfMonth(year, month) {
+    let date = new Date(year, ++month, 0).getDate();
+    return date;
+}
+
+console.log(getLastDayOfMonth(2012, 0));
+
+// ----------------------------------------
+
+// function getSecondsToday() {
+//     let date = new Date();
+//     let secInHours = date.getHours() * 3600;
+//     let secInMinutes = date.getMinutes() * 60;
+//     let currentSec = date.getSeconds();
+
+//     return secInHours + secInMinutes + currentSec;
+// }
+
+function getSecondsToday() {
+    let now = new Date();
+    let begin = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    return Math.round((now - begin) / 1000);
+}
+
+console.log(getSecondsToday());
+
+
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+    return Math.round((tomorrow - now) / 1000);
+}
+
+getSecondsToTomorrow();
